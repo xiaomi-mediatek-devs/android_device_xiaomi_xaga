cca1b3cd6a1226702f0858f86fec64d9fbcf5dd3
Aaron Kling <webgeek1234@gmail.com>
2024-09-05 19:58:33 +0100

    xaga: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+	cat <<EOF >>"$1"
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client",
+		"hardware/xiaomi"
+EOF
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
